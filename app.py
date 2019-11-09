46c0aa6bc934f98e7132b84200faa2850110e165
Narendran G <narensundaram007@gmail.com>
2019-11-09 15:20:25 +0530

    added-sample-api

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Index page."
+
+
+@app.route('/registerFC', methods=["POST"])
+def register():
+    return jsonify(request.json), 201
+
 
 if __name__ == '__main__':
     app.run()
